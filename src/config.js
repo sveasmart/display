cbb33e48979d8dfccb1af14c518283f472db4f1c
Henrik Kniberg <h@kniberg.com>
2017-06-02 03:02:47 +0200

    Added config. Buttons now work to switch tabs.

---
+++ src/config.js
@@ -0,0 +1,40 @@
+/**
+ * This module encapsulates some if the nitty gritty stuff around loading
+ * config params.
+ */
+
+var config = require('config')
+
+loadConfig("displayRpcPort")
+loadConfig("button0Tab", null)
+loadConfig("button1Tab", null)
+loadConfig("button2Tab", null)
+
+/**
+ * Loads the given config param and stores it
+ * as a field in the config object.
+ * If the param is missing, it will either return the default value (if given)
+ * or throw an exception.
+ */
+function loadConfig(paramName, defaultValue) {
+  config[paramName] = getConfig(paramName, defaultValue)
+}
+
+/**
+ * Returns the given config param.
+ * If the param is missing, it will either return the default value (if given)
+ * or throw an exception.
+ */
+function getConfig(paramName, defaultValue) {
+  if (defaultValue !== undefined) {
+    if (config.has(paramName)) {
+      return config.get(paramName)
+    } else {
+      return defaultValue
+    }
+  } else {
+    return config.get(paramName)
+  }
+}
+
+module.exports = config
--- src/start.js
+++ src/start.js
@@ -1,73 +1,90 @@
 var rpc = require('json-rpc2');
-var config = require('config')
+var config = require('./config')
+
 var display = null
 var buttons = null
 
-const displayRpcPort = config.get("displayRpcPort")
-
-var server = rpc.Server.$create({
-  'websocket': true, // is true by default
-  'headers': { // allow custom headers is empty by default
-    'Access-Control-Allow-Origin': '*'
-  }
-});
-
-// listen creates an HTTP server on localhost only 
-server.listen(displayRpcPort, 'localhost');
-
-
 function buttonPressed(buttonId) {
-  if (display) {
-    display.writeText("Button " + buttonId + " pressed")
-  }
-}
-
-try {
-  const adafruit = require('adafruit-mcp23008-ssd1306-node-driver')
-  if (adafruit.hasDriver()) {
-    console.log("Adafruit is available, so this device appears to have a display :)")
-    display = new adafruit.DisplayDriver()
-    buttons = new adafruit.ButtonDriver()
+  let tab = config["button" + buttonId + "Tab"]
+  if (tab && display) {
+    console.log("Button " + buttonId + " pressed. Showing tab " + tab + ".")
+    display.showTab(tab)
   } else {
-    display = new adafruit.FakeDisplayDriver()
-    buttons = new adafruit.FakeButtonDriver()
-    console.log("Adafruit is not available, so we'll fake the display using the console")
+    console.log("Button " + buttonId + " pressed. Ignoring it.")
   }
-} catch (err) {
-  console.log("Failed to load Adafruit, so we'll fake the display using the console" + err)
 }
 
-try {
-  if (buttons) {
-    buttons.watchAllButtons(function(buttonId) {
-      console.log("button pressed " + buttonId)
-      buttonPressed(buttonId)
-    })
+function initDisplayAndButtons() {
+  try {
+    const adafruit = require('adafruit-mcp23008-ssd1306-node-driver')
+    if (adafruit.hasDriver()) {
+      console.log("Adafruit is available, so this device appears to have a display :)")
+      display = new adafruit.DisplayDriver()
+      buttons = new adafruit.ButtonDriver()
+    } else {
+      display = new adafruit.FakeDisplayDriver()
+      buttons = new adafruit.FakeButtonDriver()
+      console.log("Adafruit is not available, so we'll fake the display using the console")
+    }
+  } catch (err) {
+    console.log("Failed to load Adafruit, so we'll fake the display using the console" + err)
   }
-} catch (err) {
-  console.log("Couldn't listen to the display buttons, so I'll skip those.", err)
 }
 
-function expose(methodName) {
-  server.expose(methodName, (args, opts, callback) => {
-    try {
-      result = display[methodName](...args)
-      callback(null, result)
-    } catch (err) {
-      callback(err)
+function listenToButtons() {
+  try {
+    if (buttons) {
+      buttons.watchAllButtons(function(buttonId) {
+        console.log("button pressed " + buttonId)
+        buttonPressed(buttonId)
+      })
+    }
+  } catch (err) {
+    console.log("Couldn't listen to the display buttons, so I'll skip those.", err)
+  }
+}
+
+function startRpcServerAndExposeDisplayMethods() {
+  const server = rpc.Server.$create({
+    'websocket': true, // is true by default
+    'headers': { // allow custom headers is empty by default
+      'Access-Control-Allow-Origin': '*'
     }
   });
 
+  // listen creates an HTTP server on localhost only
+  server.listen(config.displayRpcPort, 'localhost');
+
+  exposeDisplayMethod(server, "showTab")
+  exposeDisplayMethod(server, "clearAllTabs")
+  exposeDisplayMethod(server, "clear")
+  exposeDisplayMethod(server, "clearRow")
+  exposeDisplayMethod(server, "setQrCode")
+  exposeDisplayMethod(server, "setImage")
+  exposeDisplayMethod(server, "setTexts")
+  exposeDisplayMethod(server, "writeText")
 }
-expose("showTab")
-expose("clearAllTabs")
-expose("clear")
-expose("clearRow")
-expose("setQrCode")
-expose("setImage")
-expose("setTexts")
-expose("writeText")
 
+function exposeDisplayMethod(server, methodName) {
+  server.expose(methodName, (args, opts, callback) => {
+    if (display) {
+      try {
+        //Call the corresponoding method on the display object,
+        //and flatten out the args array into a parameter list
+        result = display[methodName](...args)
+        callback(null, result)
+      } catch (err) {
+        callback(err)
+      }
+    } else {
+      console.log("[no display] " + methodName + "(" + args.join(", ") + ")")
+      callback(null, null)
+    }
+  });
+}
 
+initDisplayAndButtons()
+listenToButtons()
+startRpcServerAndExposeDisplayMethods()
 
-console.log("Listening on port 8000")
+console.log("Display RPC server up and running on port " + config.displayRpcPort)
